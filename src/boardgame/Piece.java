639bdf9c2ba4563536dda141cbc1abb043645641
bobcamatari <bob.365consultoria@gmail.com>
2020-06-10 20:53:47 -0300

    Impelmentando movimento Roog

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,35 +1,65 @@
 package boardgame;
 
 public abstract class Piece {
-	
+
+
+
 	protected Position position;
+
 	private Board board;
+
 	
+
 	public Piece(Board board) {
+
 		this.board = board;
-		board = null;
+
+		position = null;
+
 	}
 
+
+
 	protected Board getBoard() {
+
 		return board;
+
 	}
+
 	
-	public abstract boolean[][] possibleMovies();
+
+	public abstract boolean[][] possibleMoves();
+
 	
+
 	public boolean possibleMove(Position position) {
-		return possibleMovies()[position.getRow()][position.getColumn()];
+
+		return possibleMoves()[position.getRow()][position.getColumn()];
+
 	}
+
 	
+
 	public boolean isThereAnyPossibleMove() {
-		boolean[][] mat = possibleMovies();
-		for(int i=0; i < mat.length; i++) {
-			for(int j=0; j <mat.length; j++) {
-				if(mat[i][j]) {
+
+		boolean[][] mat = possibleMoves();
+
+		for (int i=0; i<mat.length; i++) {
+
+			for (int j=0; j<mat.length; j++) {
+
+				if (mat[i][j]) {
+
 					return true;
+
 				}
+
 			}
+
 		}
+
 		return false;
+
 	}
 
 }
